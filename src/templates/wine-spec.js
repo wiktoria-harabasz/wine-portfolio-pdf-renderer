f2020c4ceaa2ee04281e6a27ca137c5b19cd501c
Wiktoria Harabasz <wiktoria.harabasz@gmail.com>
2026-08-18 17:31:01 +0100

    Create shell for wine database

--- src/templates/wine-spec.js
+++ src/templates/wine-spec.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
+const { renderWineTypeCell } = require('./producer')
 
 function renderWineSpecPage(wine) {
   const template = fs.readFileSync('./src/wine-spec-template.html', 'utf-8')
+  const typeIconHtml = renderWineTypeCell(wine)
+
   return template
     .replace('<!-- WINE_SLUG -->', wine.slug || wine._id)
+    .replace('<!-- WINE_TYPE_ICON -->', typeIconHtml)
     .replace('<!-- WINE_NAME -->', wine.wineName)
-    .replace('<!-- WINE_SUBNAME -->', wine.wineSubName || '')
+    .replace('<!-- WINE_SUBNAME -->', wine.wineSubName ? `<div class="text-h3 text-muted">${wine.wineSubName}</div>` : '')
     .replace('<!-- VINTAGE -->', wine.vintage || '')
     .replace('<!-- PRODUCER_NAME -->', wine.producerName || '')
     .replace('<!-- COUNTRY -->', wine.country || '')
@@ -17,6 +21,8 @@ function renderWineSpecPage(wine) {
     .replace('<!-- TERROIR -->', wine.terroir || '')
     .replace('<!-- WINEMAKING -->', wine.winemaking || '')
     .replace('<!-- TASTING_NOTES -->', wine.tastingNotes || '')
+    .replace('<!-- WINE_PAGE_NUMBER -->', wine.pageNumber)
+    .replace('<!-- WINE_TOTAL_PAGES -->', wine.totalPages)
 }
 
 module.exports = { renderWineSpecPage }
