2832d4a42ee9479b645c160f89a1f097db7e7db7
Wiktoria Harabasz <wiktoria.harabasz@gmail.com>
2026-09-08 20:16:01 +0200

    Add logo to cover

--- src/templates/cover.js
+++ src/templates/cover.js
@@ -3,8 +3,9 @@ function renderCoverPage(coverLabel) {
     <div style="position:relative; justify-content: center;" class="page bg-champagne text-merlot p-8 font-body flex flex-col justify-center h-full items-center" id="cover">
         <div class="flex flex-col justify-center items-center self-center text-merlot">
         <img style="margin-bottom: 40px; width: 180px; color="currentColor" src="assets/icons/nowofalowi_icon_merlot.svg"/>
-            <h1 style="font-size: 128px; letter-spacing: -1px; color: #5350E4!important;" class="font-semibold mb-4 text-merlot">Portfolio</h1>
-            <h2 style="font-size: 24px; color: #5350E4!important;" class="font-semibold capitalize text-merlot">${coverLabel || ''}</h2>
+            <h1 style="font-size: 128px; letter-spacing: -1px; color: #5350E4!important; line-height: 128px;" class="font-semibold mb-4 text-merlot">Portfolio</h1>
+            <h2 style="font-size: 40px; color: #5350E4!important;" class="font-semibold text-merlot mb-4">nowofalowi</h2>
+            <h3 style="font-size: 24px; color: #5350E4!important;" class="font-semibold capitalize text-merlot">${coverLabel || ''}</h3>
         </div>
     </div>
     `
