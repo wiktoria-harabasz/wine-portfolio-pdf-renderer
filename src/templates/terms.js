2b782ff94da2e3ac7c7a297a59473146522285cb
Wiktoria Harabasz <wiktoria.harabasz@gmail.com>
2026-09-08 20:24:03 +0200

    Change index link on terms page

--- src/templates/terms.js
+++ src/templates/terms.js
@@ -13,9 +13,12 @@ function renderTermsPage(pageNumber, totalPages, priceType) {
   return `
     <div class="page bg-champagne px-16 py-12 font-body" id="terms">
       <div class="flex flex-row justify-between w-full items-center mb-8">
-        <a href="#index" class="flex gap-2 items-center">
-            <img src="assets/icons/chevron_left.svg" style="width: 5px"/>
-            <span class="flex flex-row gap-2 items-center font-semibold text-sm text-off-black opacity-40 no-underline">
+        <a href="#index" class="flex gap-2 items-center opacity-40">
+            <div class="flex items-center">
+                <img src="assets/icons/chevron_left.svg" style="width: 5px"/>
+                <img src="assets/icons/chevron_left.svg" style="width: 5px"/>
+            </div>
+            <span class="flex flex-row gap-2 items-center text-sm text-off-black no-underline">
             Powrót do Spisu Treści
             </span>
         </a>
